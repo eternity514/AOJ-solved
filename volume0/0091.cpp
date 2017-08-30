2e4f09cdb405111f74e78c39057da5c095a1b9f5
eternity514 <0514eternity@gmail.com>
2017-08-30 22:23:22 +0900

    aoj volume0 solved problems.

--- volume0/0091.cpp
+++ volume0/0091.cpp
@@ -1,18 +1,22 @@
 #include <cstdio>
-#include <algorithm>
 
 
 int N, map[10][10], dx[3] = {0, 1, 0}, dy[3] = {1, 1, 2};
 int x[12], y[12], bl[12];
 
 
+int abs(int n){
+    if(n < 0) n = -n;
+    return n;
+}
+
 bool check(int x, int y, int t){
     int u = 1;
     if(t > 0) u = 2;
     for(int i = -u; i <= u; i++){
         for(int j = -u; j <= u; j++){
-            if(std::abs(i) + std::abs(j) > u) continue;
-            if(t == 1 && (i == 0 && std::abs(j) == 2 || j == 0 && std::abs(i) == 2)) continue;
+            if(abs(i) + abs(j) > u) continue;
+            if(t == 1 && (i == 0 && abs(j) == 2 || j == 0 && abs(i) == 2)) continue;
             if(x + j >= 10 || y + i >= 10 || x + j < 0 || y + i < 0) return false;
             if(!map[y + i][x + j]) return false;
         }
@@ -26,8 +30,8 @@ void ss(int x, int y, int t, int v){
     if(t > 0) u = 2;
     for(int i = -u; i <= u; i++){
         for(int j = -u; j <= u; j++){
-            if(std::abs(i) + std::abs(j) > u) continue;
-            if(t == 1 && (i == 0 && std::abs(j) == 2 || j == 0 && std::abs(i) == 2)) continue;
+            if(abs(i) + abs(j) > u) continue;
+            if(t == 1 && (i == 0 && abs(j) == 2 || j == 0 && abs(i) == 2)) continue;
             if(x + j >= 10 || y + i >= 10 || x + j < 0 || y + i < 0) continue;
             map[y + i][x + j] += v;
         }
